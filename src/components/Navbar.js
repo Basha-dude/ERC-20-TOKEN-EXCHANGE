3d734ff62f5cc8568a6be54f080afc84f58d2629
Basha-dude <rebelbasha99@gmail.com>
2023-05-05 23:47:54 +0530

    In Navbar.js  completed upto image without any errors

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,25 @@
 import React from 'react'
+import logo from '../assets/logo.png'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className='exchange__header grid'>
+      <div className='exchange__header--brand flex'>
+      <img src={logo} className="logo" alt="DApp Logo" />
+          <h1>Dapp Token Exchange</h1>
+          
+      </div>
+
+      <div className='exchange__header--networks flex'>
+
+      </div>
+
+      <div className='exchange__header--account flex'>
+
+      </div>
+    </div> 
+
   )
 }
 
-export default Navbar
+export default Navbar;
