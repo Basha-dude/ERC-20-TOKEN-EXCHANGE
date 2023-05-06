d8e7b68d3feff9234b0ba1425e9c95cff7fca7f7
Basha-dude <rebelbasha99@gmail.com>
2023-05-06 23:38:00 +0530

    In Navbar.js  completed upto balance and account without any errors

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,16 @@
+import { useSelector } from 'react-redux'
 import React from 'react'
 import logo from '../assets/logo.png'
 
 const Navbar = () => {
+  const account =  useSelector(state => state.provider.account)
+  const balance = useSelector(state => state.provider.balance)
   return (
     <div className='exchange__header grid'>
       <div className='exchange__header--brand flex'>
       <img src={logo} className="logo" alt="DApp Logo" />
           <h1>Dapp Token Exchange</h1>
+          
           
       </div>
 
@@ -15,7 +19,11 @@ const Navbar = () => {
       </div>
 
       <div className='exchange__header--account flex'>
-
+      {balance ?  <p><small>My Balance</small>{Number(balance).toFixed(4)}</p> : 
+       <p><small>My Balance</small>0 ETH</p>}
+     
+      {account ? <a href=""> {account.slice(0,5)+'...'+account.slice(38,42)}</a> : <a href=""></a> } 
+       
       </div>
     </div> 
 
